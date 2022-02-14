445b6bed379c3d0f09eef988d530539cccbcce55
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-02-14 11:44:36 +0530

    AllUC

---
+++ AssignmentDay33SQLQuery.sql
@@ -0,0 +1,93 @@
+create database AddressBookServiceDB
+
+use AddressBookServiceDB
+
+drop table ContactsTable
+
+create table ContactsTable(
+Id int identity(1,1) NOT NULL PRIMARY KEY,
+FirstName varchar(16) ,
+LastName varchar(16),
+Address varchar(24),
+City varchar(16),
+State varchar(16),
+ZipCode int,
+PhoneNumber varchar(16),
+EmailId varchar(24)
+);
+
+
+/* Insert contact */
+Insert into ContactsTable(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId) values 
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+('Anuj','Kumar','Rudrapur'UdhamSinghNagar','Uttarakhand','263153','9760291927','anuj.kumarr.97@gmail.com'),
+
+
+
+select * from ContactsTable
+
+/* Edit contact */
+UPDATE ContactsTable set State='Odisha' where FirstName='Sukalyan' or FirstName='Rakshi'
+
+/* Delete contact by name */
+Delete from ContactsTable where FirstName='Siba'
+
+/* Retrieve Person belonging to a City or State */
+SELECT * from ContactsTable where State='Odisha' or City='Chennai';
+
+/* Count of address book by City and State */
+select City,COUNT(City) as countOfCity from ContactsTable group by City order by City;
+select State,COUNT(State) as countOfStates from ContactsTable group by State order by State;
+
+/* sort name entries based on city */
+Insert into ContactsTable(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId) values 
+('Papuni','Dash','Electronic City','Bengaluru','Karnataka',560085,'9668443191','papuni96@gmail.com'),
+('Arul','Dash','Howrah','Kolkata','West Bengal',454435,'9530224965','arul95@mail.com'),
+('Runu','Pati','Bari','JajpurTown','Jajpur',755009,'7740329293','rp12@mail.com');
+
+SELECT * from ContactsTable
+
+SELECT * FROM ContactsTable WHERE City = 'Anandapur' ORDER BY FirstName ASC;	--ascending
+SELECT * FROM ContactsTable WHERE City = 'Anandapur' ORDER BY FirstName DESC;	--descending
+
+/* Ability to identify each Address Book with name and Type. */
+ALTER table ContactsTable ADD AdrBookName varchar(8);
+ALTER table ContactsTable ADD PersonType VARCHAR(16);
+
+UPDATE ContactsTable set AdrBookName='AB1' where Id=1 OR Id=6 OR Id=8
+UPDATE ContactsTable set AdrBookName='AB2' where Id=2 OR Id=3 
+UPDATE ContactsTable set AdrBookName='AB3' where Id=4 OR Id=7
+
+UPDATE ContactsTable set PersonType='Friends' where AdrBookName='AB1'
+UPDATE ContactsTable set PersonType='Family' where AdrBookName='AB2'
+UPDATE ContactsTable set PersonType='Profession' where AdrBookName='AB3'
+
+SELECT * FROM ContactsTable
+
+/* Ability to get number of contact persons i.e. count by type */
+SELECT PersonType,COUNT(PersonType) as count from ContactsTable group by PersonType
+
+/* Ability to add person to both Friend and Family */
+create table PersonCategory (
+	Id int identity(1,1) primary key,
+	PersonId int foreign key references ContactsTable(Id),
+	PersonType varchar(20) not null
+	)
+insert into PersonCategory(PersonId,PersonType) values (1,'Family'),(2,'Friend'),(3,'Family'),(4,'Friend'),(6,'Profession'),(7,'Profession'),(8,'Profession');
+select * from PersonCategory;
+----- adding id 4 as friend and family
+insert into PersonCategory(PersonId,PersonType) values(1,'Profession'),(4,'Family');
+
+/* ER diagram and normalisation */
+Alter Table ContactsTable drop column PersonType
+select * from ContactsTable inner join PersonCategory on ContactsTable.Id=PersonCategory.PersonId
+
+/* Test queries */
+SELECT * FROM ContactsTable WHERE City = 'Anandapur' ORDER BY FirstName ASC;	--ascending
+SELECT * from ContactsTable where State='Odisha' or City='Chennai';
+SELECT City,COUNT(City) as countOfCity from ContactsTable group by City order by City;
+SELECT PersonType,COUNT(PersonType) as count from ContactsTable group by PersonType;
